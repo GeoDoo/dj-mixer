e05d5728ba77b78871f3672e92b7bebf54746fb8
Georgios Karametas <geometas@hotmail.com>
2026-07-18 19:28:36 +0100

    explicit timePitch init: overlap=8, pitch=0, rate=1.0

--- Sources/dj-mixer/dj_mixer.swift
+++ Sources/dj-mixer/dj_mixer.swift
@@ -219,6 +219,7 @@ enum FXBeat: String, CaseIterable { case whole = "1/1", half = "1/2", quarter = 
     init(id: Int) {
         self.id = id
         eq = AVAudioUnitEQ(numberOfBands: 3)
+        timePitch.overlap = 8; timePitch.pitch = 0; timePitch.rate = 1.0
         let cfgs: [(AVAudioUnitEQFilterType, Float, Float)] = [(.highShelf, 7000, 0.5), (.parametric, 1200, 0.7), (.lowShelf, 200, 0.5)]
         for (i, (t, f, b)) in cfgs.enumerated() {
             eq.bands[i].filterType = t; eq.bands[i].frequency = f
