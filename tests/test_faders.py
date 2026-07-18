a0efacb5845492d530e65014b3f0e8e47a58d9b2
Georgios Karametas <geometas@hotmail.com>
2026-07-18 15:07:05 +0100

    impeccable critique: button depth, fader caps, section dividers, 96px waveform, solid borders

--- tests/test_faders.py
+++ tests/test_faders.py
@@ -2,7 +2,7 @@
 def test_eq_is_fader_not_knob():
     html = open('index.html').read()
     assert 'eq-fader' in html, 'no EQ fader elements'
-    assert 'class=\"knob\"' not in html, 'rotary knob CSS still present'
+    assert '"knob"' not in html, 'rotary knob CSS still present'
 
 def test_vol_is_fader_not_knob():
     html = open('index.html').read()
